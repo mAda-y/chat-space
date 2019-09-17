a17583f52681416c89301767052c7f7839963125
yamadamaya513 <yunaray513@gmail.com>
2019-09-17 14:30:55 +0900

    7commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages''#index'
+  root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups,onry: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]  
