9b10d17557bf2b2d40a90a0bcac648b7921deb17
yamadamaya513 <yunaray513@gmail.com>
2019-09-17 10:33:44 +0900

    4commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages''#index'
   resources :users, only: [:edit, :update]
+  resources :groups,onry: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
+end  
